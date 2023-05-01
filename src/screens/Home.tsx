4d5413fbc61fb93a0afa974e8c0cefd8d53265da
Sunil Bamre <sunil.bamre@gmail.com>
2023-05-01 10:54:01 +0530

    project setup created

---
+++ src/screens/Home.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
+  
+
+function Home() {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+});
+ 
+export default Home;
